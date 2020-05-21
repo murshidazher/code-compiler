b1b937eb8b6a140aa1b46936f23ff948f01c808c
Murshid Azher <murshid@atera.io>
2020-05-21 10:54:43 +0530

    feat(main): add main csharp

--- python.py
+++ python.py
@@ -1,1 +1,7 @@
-print("hello [[[jnjn]]] !"); print(2+4);
+rows = int(input("enter number of rows "))
+b = 0
+for i in range(rows, 0, -1):
+    b += 1
+    for j in range(1, i + 1):
+        print(b, end=' ')
+    print('\r')
--- samples/shapes/Main.cs
+++ samples/shapes/Main.cs
@@ -18,10 +18,13 @@ public class HelloWorld {
 
         for(i=0;i<n;i++)
         {
-            for(j=1;j<=n-i;j++)
-            Console.Write(" ");
-            for(j=1;j<=2*i-1;j++)
-            Console.Write("*");
+            for(j=1;j<=n-i;j++){
+                Console.Write(" ");
+            }
+            for(j=1;j<=2*i-1;j++) {
+                Console.Write("*");
+            }
+                
             Console.Write("\n");
         }
     }
