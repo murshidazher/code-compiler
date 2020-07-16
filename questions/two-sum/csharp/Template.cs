104d3c20fb92e800cfd4471d11d993852cc4e899
murshidazher <murshidazher@yahoo.com>
2020-07-16 13:19:24 +0530

    feat(template): add csharp solution template

---
+++ questions/two-sum/csharp/Template.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+public class Solution
+{
+	public virtual int[] twoSum(int[] nums, int target)
+	{
+		// Add your solution inside this function
+		return new int[]{0, 0};
+	}
+}
