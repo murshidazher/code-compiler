ad214633a8e34a28aa6bf11373fb35b65101018f
murshidazher <murshidazher@yahoo.com>
2020-07-16 12:24:54 +0530

    feat(SolutionTester): add csharp solution tester

--- questions/two-sum/csharp/SolutionTester.cs
+++ questions/two-sum/csharp/SolutionTester.cs
@@ -1,1 +1,69 @@
-// implement the solutiontester file
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class SolutionTester
+{
+	private const string TESTCASE_FILE = "../testcase.txt";
+
+	public static void Main(string[] args)
+	{
+		Solution solution = new Solution();
+		List<string> testCaseResults = new List<string>();
+
+		bool testResult = true;
+		try
+		{
+			StreamReader br = new StreamReader(TESTCASE_FILE);
+			try
+			{
+				string line;
+				int count = 0;
+				while (!string.ReferenceEquals((line = br.ReadLine()), null))
+				{
+					int[] nums = ParserUtil.stringToIntegerArray(line);
+					line = br.ReadLine();
+					int target = int.Parse(line);
+					line = br.ReadLine();
+					int[] expected = ParserUtil.stringToIntegerArray(line);
+					// create an test execution
+					int[] ret = solution.twoSum(nums, target);
+
+					testResult = Enumerable.SequenceEqual(expected, ret);
+					count++;
+
+					if (!testResult)
+					{
+						testCaseResults.Add("[Fail] " + " [Input] " + "[" + string.Join(", ", nums) + "]" + ", " + target + "  [Returned] " + "[" + string.Join(", ", ret) + "]" + " [Expected] " + "[" + string.Join(", ", expected) + "]");
+					}
+					else
+					{
+						testCaseResults.Add("[Success] Your solution passed");
+					}
+				}
+			}
+			catch (Exception io)
+			{
+				Console.WriteLine(io.Message);
+			}
+			finally
+			{
+				br.Close();
+			}
+		}
+		catch (IOException ioe)
+		{
+			Console.WriteLine(ioe.Message);
+
+		}
+		finally
+		{
+			foreach (object i in testCaseResults)
+			{
+				Console.WriteLine(i);
+			}	
+		}
+	}
+
+}
