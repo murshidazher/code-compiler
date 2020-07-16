cca64592b04f84bcd9af015eb446c039cb19f353
murshidazher <murshidazher@yahoo.com>
2020-07-16 11:42:33 +0530

    feat(solution): add real solution

--- questions/two-sum/csharp/RealSolution.cs
+++ questions/two-sum/csharp/RealSolution.cs
@@ -4,6 +4,27 @@ public class Solution
 {
 	public virtual int[] twoSum(int[] nums, int target)
 	{
-		// the solution goes here
+		int[] res = new int[]{0, 0};
+		if (nums == null || nums.Length < 2)
+		{
+			return res;
+		}
+
+		Dictionary<int, int> map = new Dictionary<int, int>();
+
+		for (int i = 0; i < nums.Length; i++)
+		{
+			if (map.ContainsKey(nums[i]))
+			{
+				res[0] = map[nums[i]];
+				res[1] = i;
+				return res;
+			}
+			else
+			{
+				map[target - nums[i]] = i;
+			}
+		}
+		return res;
 	}
 }
