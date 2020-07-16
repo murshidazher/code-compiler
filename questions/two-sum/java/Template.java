06225c5afb12a9539bc1151eb26d55be2ae7354e
murshidazher <murshidazher@yahoo.com>
2020-07-16 13:17:10 +0530

    feat(template): add java solution template

---
+++ questions/two-sum/java/Template.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // Add your solution inside this function
+        return [0, 0];
+    }
+}
