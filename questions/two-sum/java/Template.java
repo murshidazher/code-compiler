f36ab3c11df073931d0d71a66f65cd80dea0ac80
murshidazher <murshidazher@yahoo.com>
2020-07-16 13:19:09 +0530

    fix(template): fix the return arr of template

--- questions/two-sum/java/Template.java
+++ questions/two-sum/java/Template.java
@@ -3,6 +3,6 @@ import java.util.HashMap;
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
         // Add your solution inside this function
-        return [0, 0];
+        return new int[]{0,0};
     }
 }
